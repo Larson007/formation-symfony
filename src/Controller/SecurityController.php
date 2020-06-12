9b58eb683085c5c2cab405f61cf7f402dafa73ac
Larson007 <mohamed.benallal.pro@gmail.com>
2020-06-12 23:37:37 +0200

    Gestion de l'authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PasswordUpdate;
+use App\Form\AccountType;
+use App\Form\PasswordUpdateType;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * permet d'editer un profile User
+     * 
+     * @Route("account/profile", name="app_profile")
+     *
+     * @return Response
+     */
+    public function profile(Request $request, ObjectManager $manager)
+    {
+        $user = $this->getUser();                               // Permet de recuprer l'utilisateur actuelement connecté
+
+        $form = $this->createForm(AccountType::class, $user);   // Import du formualaire générer d'edition
+
+        $form -> handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les modifications ont bien été enregistrés"
+            );
+        }
+
+        return $this->render('security/profile.html.twig', [    // Chemin vers le render twig
+            'form' => $form->createView()                       // On passe le formluaire au template Twig pour l'afficher
+        ]);
+    }
+
+    /**
+     * Permet de modifier le MDP
+     * 
+     * @Route("/account/password-update", name="app_password-update")
+     *
+     * @return Response
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, ObjectManager $manager)
+    {   //  Instancie la variable passwordUpdate a partir de la fausse Entity PasswordUpdate
+        $passwordUpdate = new PasswordUpdate;
+
+        // On importe l'utilisateur qui est actuellement connecté
+        $user = $this->getUser();
+
+        //  On dde a symfony de créée un formulaire a partir fichier FORM/PasswordUpdateType avec les donnée de l'instantiation de PasswordUpdate
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        //  On instancie une Requête pour communiquer avec la BDD importer depuis Request et stocker dans $request 
+        $form->handleRequest($request);
+
+        // On vérifie que l'utilisateur a bien respecter les @Assert de la fausse Entity PasswordUpdate
+        if($form->isSubmitted() && $form->isValid()){
+
+            // Vérifier que l'ancien MDP soit le même que celui de l'utilisateur actuellement connecté via la fonction PHP  qui compare le MDP Non Hasher et Hasher : password_verify(NoHash, Hash)
+            if(!password_verify($passwordUpdate->getOldPassword(), $user->getPassword())){ // le ! permet d'inverser la question (si le MDP no HASH N'EST PAS = MDP HASH)
+                
+                //  GESTION DES ERREURS :
+                //  $form->get('#') permet de cibler un champs précis de notre formualaire
+                //  addError permet de faire passer un msg personnaliser en cas d'erreur 
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez tapper n'est pas votre mot de passe actuel"));
+
+                // SI OK : MDP noHASH = MDP HASH
+            }   else {
+                $newPassword = $passwordUpdate->getNewPassword();
+
+                //  On lui dit d'encoder le new MDP dans le MDP actuel
+                $password = $encoder->encodePassword($user, $newPassword);
+
+                // On envoi a l'Entity User le New MDP a la place de l'ancien 
+                $user->setPassword($password);
+
+                //  On sauvegarde les changements
+                $manager->persist($user);
+
+                //  On envoi les nvlle donnée ds la BDD
+                $manager->flush();
+
+                // On affiche un msg si tous a bien fonctionner
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifié"
+                );
+
+                // On fini par rediriger vers la page d'accueil
+                return $this->redirectToRoute('homepage');
+            } 
+        }
+
+        return $this->render('security/password.html.twig', [
+            'form' => $form->createView()                       // On oublie pas d'envoyer le formulaire stocker dans $form vers le template ! 
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,18 +1,63 @@
 <?php
 namespace App\DataFixtures;
 
-use App\Entity\Ad;
 use Faker\Factory;
-use App\Entity\Image;                                                  // La class Image importer fais bien référence à l'Entity Image
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;   // Import de UserPasswordEncoderInterface
+use App\Entity\Ad;                                                          // Import de l'Entity Ad
+use App\Entity\User;                                                        // Import de l'Entity User
+use App\Entity\Image;                                                       // Import de l'Entity Image
 
 class AppFixtures extends Fixture
 {
+    private $encoder;                                               // On créer une propriété $encoder en private
+
+    public function __construct(UserPasswordEncoderInterface $encoder )
+    {
+        $this->encoder = $encoder;                                  // On stock la propriété private $encoder dans la fonction public $encoder pour l'utiliser dans tt les function du fichier Fixture AppFixtures
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
 
+        // Nous gérons les utilisateur
+        $users = [];                                                // On déclare un tableau vide $users qui contiendra les x10 $user créer
+
+        $genres = ['male', 'female'];                               // On déclare un tableau $genres pour 'Hommes' ou 'Femmes' en Anglais pour utiliser $faker
+
+        for ($i=1; $i < 10; $i++) { 
+            $user = new User();                                     // Ne pas oublier d'importer la class Entity User
+
+            $genre = $faker->randomElement($genres);                // $faker va récuprer un élémént aléatoire du tableau $genres
+            
+            $picture = 'https://randomuser.me/api/portraits/';      // On utilise le site randomuser.me en prenant l'adresse qui contient les avatars ( https://randomuser.me/api/portraits/men/68.jpg )
+            $pictureId = $faker->numberBetween(1, 99) . '.jpg' ;    // le site randomuser.me propose 99 avatar Hommes et Femmes
+
+            // Sans condition ternaire :
+//            if ($genre == "male") $picture = $picture . 'men/' . $pictureId;    // On mets une condition si le genre est 'male' on concatenant une Url vers l'avatar d'un Homme.
+//            else $picture = $picture . 'women/' . $pictureId;                   // Idem mais pour une Femme.
+
+            // Avec Condition ternaire :
+            $picture .= ($genre == 'male' ? 'men/' : 'women/') . $pictureId;
+
+            // la méthode encodePassword viens de la class UserPasswordEncoderInterface stocker dans 'encoder' et encode le MDP avec l'encodeur choisir dans security.yaml
+            $password = $this->encoder->encodePassword($user, 'password');  // encodePassword prend 2 paramettres (l'Entity cible , le MDP à encoder ) et va retouner le 2ieme paramettre hasher dans $hash
+
+            $user   #-> setFirstName($faker->firstname($genre))      // $faker va donner un nom/prenom en fonction du genre
+                    #-> setLastname($faker->lastName($genre))
+                    -> setEmail($faker->email)
+                    #-> setIntroduction($faker->sentence())
+                    #-> setDescription($faker->paragraph(3))
+                    -> setPassword($password);                               // On définit le champs MDP via la variable $hash construite plus tôt 
+                    #-> setPicture($picture);                        // On appel notre condition $picture qui gére les avatar par genre via randomuser.me
+
+            $manager->persist($user);                               // On dde a sauvegarder x10 nos $user générer via $faker
+            $users [] = $user;                                      // On place les $user dans le tableau vide $users créer précédement
+        }
+
+        // Nous gérons les annonces
         for($i = 1; $i <= 30; $i++){
             $ad = new Ad();
 
@@ -21,12 +66,15 @@ class AppFixtures extends Fixture
             $introduction   = $faker->paragraph(2);
             $content        = $faker->paragraph(5);
 
+            $user = $users [mt_rand(0, count($users) -1)];           // On donne à nos annonces un auteur générer de 1 aux max-1 au cas ou on a + de 10 user par la suite du tableau $users
+
             $ad ->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1, 5));
+                ->setRooms(mt_rand(1, 5))
+                ->setAuthor($user);                                 // On previens l'annonce qu'elle appartient a un $user
 
                                                                     // On va boucler a nouveau pour les images
             for ($j=1; $j < mt_rand(2, 5); $j++) {                  // $j = 1 pour min 1 image | $j < mt_rand(2, 5) pour un max entre 2 et 5 image | $j++ on incrémente jusu'au max
@@ -35,13 +83,11 @@ class AppFixtures extends Fixture
                 $image  ->setUrl($faker->imageUrl())                // On utilise Faker pour générer des url d'images
                         ->setCaption($faker->sentence())            // Faker génere une légende (alt de la balise <img>)
                         ->setAd($ad);                               // Pour la lier l'image à l'annonce qui lui Ad qui lui est lié
-                
+
                 $manager->persist($image);                          // on persite l'image (la faire perdurer dans le tps dans la BDD)
             }
-
             $manager->persist($ad);
         }
-
         $manager->flush();
     }
 }
